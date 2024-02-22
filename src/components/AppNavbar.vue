00181a0fad7ba2a97270f7025eeb880673fcd98c
Kristian Seman <78860937+xseman98@users.noreply.github.com>
2024-02-22 16:04:55 +0100

    feat: add navbar component

---
+++ src/components/AppNavbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-toolbar>
+    <div class="navbar">
+      <img alt="Webstore logo" src="../assets/logo.svg" width="24" />
+      <v-toolbar-title>Webstore</v-toolbar-title>
+    </div>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  name: "AppNavbar",
+};
+</script>
+
+<style scoped>
+.navbar {
+  height: 65px;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 1rem;
+}
+</style>
