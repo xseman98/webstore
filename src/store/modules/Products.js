4fade5b3d366f628c9ece392030b586a30f4dbc4
Kristian Seman <78860937+xseman98@users.noreply.github.com>
2024-02-23 14:36:19 +0100

    feat: add products store getters

--- src/store/modules/Products.js
+++ src/store/modules/Products.js
@@ -1,14 +1,49 @@
 const state = {
+  didLoadedProducts: false,
   products: {},
 };
 
 const getters = {
-  getProducts: (state) => state.products,
+  didLoadedProducts: (state) => state.didLoadedProducts,
+
+  getItems: (state) => state.products.items,
+
+  getItemsByCategory: (state) => (categoryName) => {
+    console.log(categoryName, state.products.items);
+    if (categoryName === "All") return state.products.items;
+    const filteredProducts = [];
+    const items = state.products.items;
+
+    items.forEach((item) => {
+      const categories = item.categories.map((category) => category.name);
+      if (categories.includes(categoryName)) {
+        filteredProducts.push(item);
+      }
+    });
+
+    return filteredProducts;
+  },
+
+  getNumberOfItems: (state) => state.products.total_count,
+
+  getAvailableCategories: (state) => {
+    const categoriesSet = new Set(["All"]);
+    const items = state.products.items;
+
+    items.forEach((item) => {
+      item.categories.forEach((category) => {
+        categoriesSet.add(category.name);
+      });
+    });
+
+    return Array.from(categoriesSet);
+  },
 };
 
 const actions = {};
 
 const mutations = {
+  setDidLoadedProducts: (state, value) => (state.didLoadedProducts = value),
   setProducts: (state, products) => (state.products = products),
 };
 
