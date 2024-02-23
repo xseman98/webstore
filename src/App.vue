d4a3251df08bf8c55e561215bbc27d485da75748
Kristian Seman <78860937+xseman98@users.noreply.github.com>
2024-02-23 09:40:32 +0100

    feat: use gql query to fetch data

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,42 @@
 <template>
   <v-app>
-    <v-main>
-      <app-navbar></app-navbar>
+    <app-loader v-if="$apollo.loading" />
+    <v-main v-else>
+      <app-navbar />
       <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
+import { PRODUCTS_QUERY } from "./graphql";
+import AppLoader from "./components/AppLoader.vue";
 import AppNavbar from "./components/AppNavbar.vue";
 
 export default {
   name: "App",
 
   components: {
+    AppLoader,
     AppNavbar,
   },
 
-  data: () => ({
-    //
-  }),
+  data() {
+    return {
+      products: {},
+    };
+  },
+
+  apollo: {
+    products: {
+      query: PRODUCTS_QUERY,
+      variables() {
+        return {
+          search: "",
+        };
+      },
+      update: (data) => data.products,
+    },
+  },
 };
 </script>
