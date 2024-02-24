4760925b0e35745157c3686d77dc774ddc74b518
Kristian Seman <78860937+xseman98@users.noreply.github.com>
2024-02-24 19:07:13 +0100

    feat: export new query

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,4 +1,5 @@
 import { PRODUCTS_QUERY } from "./productsQuery";
 import { PAGE_QUERY } from "./productsPageQuery";
+import { CATEGORIES_QUERY } from "./categoriesQuery";
 
-export { PRODUCTS_QUERY, PAGE_QUERY };
+export { PRODUCTS_QUERY, PAGE_QUERY, CATEGORIES_QUERY };
