e86d982ca252370d8ad6bfffda6070dd585daab5
Kristian Seman <78860937+xseman98@users.noreply.github.com>
2024-02-24 19:08:00 +0100

    feat: simplify products store

--- src/store/modules/Products.js
+++ src/store/modules/Products.js
@@ -1,48 +1,15 @@
 const state = {
-  didLoadedProducts: false,
   products: {},
 };
 
 const getters = {
-  didLoadedProducts: (state) => state.didLoadedProducts,
-
   getItems: (state) => state.products.items,
-
-  getItemsByCategory: (state) => (categoryName) => {
-    if (categoryName === "All") return state.products.items;
-    const filteredProducts = [];
-    const items = state.products.items;
-
-    items.forEach((item) => {
-      const categories = item.categories.map((category) => category.name);
-      if (categories.includes(categoryName)) {
-        filteredProducts.push(item);
-      }
-    });
-
-    return filteredProducts;
-  },
-
   getNumberOfItems: (state) => state.products.total_count,
-
-  getAvailableCategories: (state) => {
-    const categoriesSet = new Set(["All"]);
-    const items = state.products.items;
-
-    items.forEach((item) => {
-      item.categories.forEach((category) => {
-        categoriesSet.add(category.name);
-      });
-    });
-
-    return Array.from(categoriesSet);
-  },
 };
 
 const actions = {};
 
 const mutations = {
-  setDidLoadedProducts: (state, value) => (state.didLoadedProducts = value),
   setProducts: (state, products) => (state.products = products),
 };
 
