6712cf44e2a119aa252ee1ec6f4787a1bf0f5770
Kristian Seman <78860937+xseman98@users.noreply.github.com>
2024-02-24 20:19:54 +0100

    feat: products store update

--- src/store/modules/Products.js
+++ src/store/modules/Products.js
@@ -1,16 +1,35 @@
 const state = {
   products: {},
+  filters: {
+    apply: false,
+    data: {},
+  },
 };
 
 const getters = {
-  getItems: (state) => state.products.items,
-  getNumberOfItems: (state) => state.products.total_count,
+  getItems: (state) => {
+    if (!state.filters.apply) return state.products.items;
+
+    const stockStatus = state.filters.data.availability;
+    const priceFrom = state.filters.data.priceRange[0];
+    const priceTo = state.filters.data.priceRange[1];
+
+    return state.products.items.filter((item) => {
+      return !stockStatus
+        ? item.price_range.maximum_price.final_price.value >= priceFrom &&
+            item.price_range.maximum_price.final_price.value <= priceTo
+        : item.stock_status === stockStatus &&
+            item.price_range.maximum_price.final_price.value >= priceFrom &&
+            item.price_range.maximum_price.final_price.value <= priceTo;
+    });
+  },
 };
 
 const actions = {};
 
 const mutations = {
   setProducts: (state, products) => (state.products = products),
+  setFilters: (state, filters) => (state.filters = filters),
 };
 
 export default {
