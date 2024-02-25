f877c670cde28d7ccf0a671a80e8de7289090c7f
Kristian Seman <78860937+xseman98@users.noreply.github.com>
2024-02-25 17:27:49 +0100

    feat: update apollo definition

--- src/plugins/apollo.js
+++ src/plugins/apollo.js
@@ -1,21 +1,16 @@
 import Vue from "vue";
 import VueApollo from "vue-apollo";
-import { ApolloClient } from "apollo-client";
+import { ApolloClient, InMemoryCache } from "@apollo/client/core";
 import { createHttpLink } from "apollo-link-http";
-import { InMemoryCache } from "apollo-cache-inmemory";
 
 Vue.use(VueApollo);
 
-// HTTP connection to the API
 const httpLink = createHttpLink({
-  // You should use an absolute URL here
-  uri: process.env.VUE_APP_GRAPHQL_HTTP,
+  uri: `http://localhost:${process.env.VUE_APP_PORT}/graphql`,
 });
 
-// Cache implementation
 const cache = new InMemoryCache();
 
-// Create the apollo client
 const apolloClient = new ApolloClient({
   link: httpLink,
   cache,
