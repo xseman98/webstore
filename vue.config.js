68cda4c592b9f9aceaa6f146c0d9ad1014abb90a
Kristian Seman <78860937+xseman98@users.noreply.github.com>
2024-02-25 17:28:03 +0100

    feat: setup proxy

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: ["vuetify"],
+  devServer: {
+    port: process.env.VUE_APP_PORT,
+    proxy: {
+      "/graphql": {
+        target: process.env.VUE_APP_VENIA_URL,
+        changeOrigin: true,
+        pathRewrite: {
+          "^/graphql": "/graphql",
+        },
+      },
+    },
+  },
 });
