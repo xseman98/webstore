2013c3e01ce2707da2ef84ddcc095c7d399f7e7b
Kristian Seman <78860937+xseman98@users.noreply.github.com>
2024-02-25 17:28:24 +0100

    feat: update categories query

--- src/graphql/categoriesQuery.js
+++ src/graphql/categoriesQuery.js
@@ -2,7 +2,7 @@ import gql from "graphql-tag";
 
 export const CATEGORIES_QUERY = gql`
   query Categories($search: String) {
-    products(search: $search) {
+    products(search: $search, pageSize: 10000) {
       items {
         categories {
           uid
