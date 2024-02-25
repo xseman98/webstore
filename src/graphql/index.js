a3dce20205b0fbac75f408afc04ee6839c1caf09
Kristian Seman <78860937+xseman98@users.noreply.github.com>
2024-02-25 19:04:55 +0100

    chore: cleaning up

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,5 +1,4 @@
 import { PRODUCTS_QUERY } from "./productsQuery";
-import { PAGE_QUERY } from "./productsPageQuery";
 import { CATEGORIES_QUERY } from "./categoriesQuery";
 
-export { PRODUCTS_QUERY, PAGE_QUERY, CATEGORIES_QUERY };
+export { PRODUCTS_QUERY, CATEGORIES_QUERY };
